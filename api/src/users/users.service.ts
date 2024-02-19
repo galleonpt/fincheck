7935ba408c016bc39206a7006d7c1998279d93fb
galleonpt <jose15.luis00@gmail.com>
2024-02-19 23:17:49 +0000

    feat: hammer categories upon creating an user

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -25,9 +25,69 @@ export class UsersService {
                 name,
                 email,
                 password: hashedPw,
+                categories: {
+                    createMany: {
+                        data: [
+                            // Income
+                            {
+                                name: 'Salary',
+                                icon: 'salary',
+                                type: 'INCOME',
+                            },
+                            {
+                                name: 'Freelance',
+                                icon: 'freelance',
+                                type: 'INCOME',
+                            },
+                            {
+                                name: 'Other',
+                                icon: 'other',
+                                type: 'INCOME',
+                            },
+                            // Expense
+                            { name: 'Home', icon: 'home', type: 'EXPENSE' },
+                            {
+                                name: 'Food',
+                                icon: 'food',
+                                type: 'EXPENSE',
+                            },
+                            {
+                                name: 'Education',
+                                icon: 'education',
+                                type: 'EXPENSE',
+                            },
+                            { name: 'Fun', icon: 'fun', type: 'EXPENSE' },
+                            {
+                                name: 'Grocery',
+                                icon: 'grocery',
+                                type: 'EXPENSE',
+                            },
+                            {
+                                name: 'Clothes',
+                                icon: 'clothes',
+                                type: 'EXPENSE',
+                            },
+                            {
+                                name: 'Transport',
+                                icon: 'transport',
+                                type: 'EXPENSE',
+                            },
+                            {
+                                name: 'Travel',
+                                icon: 'travel',
+                                type: 'EXPENSE',
+                            },
+                            {
+                                name: 'Other',
+                                icon: 'other',
+                                type: 'EXPENSE',
+                            },
+                        ],
+                    },
+                },
             },
         });
 
-        return user;
+        return { name: user.name, email: user.email };
     }
 }
