318da9a8c649570d78677b0fec0ab8316939e62e
galleonpt <jose15.luis00@gmail.com>
2024-02-22 00:38:45 +0000

    feat: app authentication

--- api/src/modules/auth/auth.service.ts
+++ api/src/modules/auth/auth.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthenticateDto } from './dto/authenticate.dto';
+import { UsersRepository } from 'src/shared/database/repositories/users.repository';
+import { compare } from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly usersRepository: UsersRepository,
+        private readonly jwtService: JwtService,
+    ) {}
+
+    async authenticate({ email, password }: AuthenticateDto) {
+        const user = await this.usersRepository.findUnique({
+            where: { email },
+        });
+
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const isPasswordValid = await compare(password, user.password);
+
+        if (!isPasswordValid) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const payload = { sub: user.id };
+        const accessToken = await this.jwtService.signAsync(payload);
+
+        return {
+            accessToken,
+        };
+    }
+}
