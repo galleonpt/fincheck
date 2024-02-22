8b238ebffe3f79596b6c698c8c6ad19af83a6ab4
galleonpt <jose15.luis00@gmail.com>
2024-02-22 00:58:37 +0000

    move account creation to auth module

--- api/src/modules/auth/auth.service.ts
+++ api/src/modules/auth/auth.service.ts
@@ -1,8 +1,14 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { AuthenticateDto } from './dto/authenticate.dto';
-import { UsersRepository } from 'src/shared/database/repositories/users.repository';
-import { compare } from 'bcryptjs';
+import {
+    ConflictException,
+    Injectable,
+    UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { compare, hash } from 'bcryptjs';
+
+import { UsersRepository } from 'src/shared/database/repositories/users.repository';
+import { AuthenticateDto } from './dto/authenticate.dto';
+import { SignUpDto } from './dto/signup.dto';
 
 @Injectable()
 export class AuthService {
@@ -11,7 +17,7 @@ export class AuthService {
         private readonly jwtService: JwtService,
     ) {}
 
-    async authenticate({ email, password }: AuthenticateDto) {
+    async signin({ email, password }: AuthenticateDto) {
         const user = await this.usersRepository.findUnique({
             where: { email },
         });
@@ -32,5 +38,88 @@ export class AuthService {
         return {
             accessToken,
         };
+    }
+
+    async signup({ name, email, password }: SignUpDto) {
+        const emailAlreadyInUse = await this.usersRepository.findUnique({
+            where: { email },
+            select: { id: true },
+        });
+
+        if (emailAlreadyInUse) {
+            throw new ConflictException('This email is already in use.');
+        }
+
+        const hashedPw = await hash(password, 12);
+
+        const user = await this.usersRepository.create({
+            data: {
+                name,
+                email,
+                password: hashedPw,
+                categories: {
+                    createMany: {
+                        data: [
+                            // Income
+                            {
+                                name: 'Salary',
+                                icon: 'salary',
+                                type: 'INCOME',
+                            },
+                            {
+                                name: 'Freelance',
+                                icon: 'freelance',
+                                type: 'INCOME',
+                            },
+                            {
+                                name: 'Other',
+                                icon: 'other',
+                                type: 'INCOME',
+                            },
+                            // Expense
+                            { name: 'Home', icon: 'home', type: 'EXPENSE' },
+                            {
+                                name: 'Food',
+                                icon: 'food',
+                                type: 'EXPENSE',
+                            },
+                            {
+                                name: 'Education',
+                                icon: 'education',
+                                type: 'EXPENSE',
+                            },
+                            { name: 'Fun', icon: 'fun', type: 'EXPENSE' },
+                            {
+                                name: 'Grocery',
+                                icon: 'grocery',
+                                type: 'EXPENSE',
+                            },
+                            {
+                                name: 'Clothes',
+                                icon: 'clothes',
+                                type: 'EXPENSE',
+                            },
+                            {
+                                name: 'Transport',
+                                icon: 'transport',
+                                type: 'EXPENSE',
+                            },
+                            {
+                                name: 'Travel',
+                                icon: 'travel',
+                                type: 'EXPENSE',
+                            },
+                            {
+                                name: 'Other',
+                                icon: 'other',
+                                type: 'EXPENSE',
+                            },
+                        ],
+                    },
+                },
+            },
+        });
+
+        return { name: user.name, email: user.email };
     }
 }
