0e8d4886cf5675c28548910f0f6449a965da78d7
galleonpt <jose15.luis00@gmail.com>
2024-02-22 01:48:57 +0000

    feat: auth guard and protect routes

--- api/src/modules/users/users.service.ts
+++ api/src/modules/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { UsersRepository } from 'src/shared/database/repositories/users.repository';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private readonly usersRepository: UsersRepository) {}
+
+    async getUserById(userId: string): Promise<Pick<User, 'name' | 'email'>> {
+        return this.usersRepository.findUnique({
+            where: {
+                id: userId,
+            },
+            select: {
+                name: true,
+                email: true,
+            },
+        });
+    }
+}
