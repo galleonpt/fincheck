4ee99c0621ebde6b6c4b642b26fed13b718905f3
galleonpt <jose15.luis00@gmail.com>
2024-03-02 22:14:07 +0000

    refactor(router): use new components

--- frontend/src/Router/router.tsx
+++ frontend/src/Router/router.tsx
@@ -1,17 +1,20 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import AuthGuard from './AuthGuard';
+import Login from '../view/pages/Login';
+import Register from '../view/pages/Register';
+import Dashboard from '../view/pages/Dashboard';
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<AuthGuard isPrivate={false} />}>
-                    <Route path="/login" element={<>Login</>} />
-                    <Route path="/register" element={<>Register</>} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
                 </Route>
 
                 <Route element={<AuthGuard isPrivate />}>
-                    <Route path="/" element={<>Dashboard</>} />
+                    <Route path="/" element={<Dashboard />} />
                 </Route>
             </Routes>
         </BrowserRouter>
