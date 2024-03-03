cdcf5a0e02c70a9b8fdc6bd01699c5f1b3da5b92
galleonpt <jose15.luis00@gmail.com>
2024-03-03 01:32:56 +0000

    feat: input component

--- frontend/src/view/pages/Login/Login.tsx
+++ frontend/src/view/pages/Login/Login.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
+import Input from '../../components/Input';
 
 const Login: FC = () => {
     return (
@@ -22,6 +23,15 @@ const Login: FC = () => {
                     </Link>
                 </p>
             </header>
+
+            <form className="mt-[60px] flex flex-col gap-4">
+                <Input type="email" placeholder="E-mail" name="email" />
+                <Input type="password" placeholder="Password" name="password" />
+
+                <button type="submit" className="mt-2">
+                    Entrar
+                </button>
+            </form>
         </>
     );
 };
