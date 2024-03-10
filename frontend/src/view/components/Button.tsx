d5c673985ef0f7f91ce19aeb7f2c68fe2c2bce68
galleonpt <jose15.luis00@gmail.com>
2024-03-10 14:35:45 +0000

    refactor(button): allow button to override classname

--- frontend/src/view/components/Button.tsx
+++ frontend/src/view/components/Button.tsx
@@ -1,12 +1,16 @@
 import { ComponentProps, FC } from 'react';
+import cn from '../../app/utils/cn';
 
 interface IButtonProps extends ComponentProps<'button'> {}
 
-const Button: FC<IButtonProps> = ({ children, ...props }) => {
+const Button: FC<IButtonProps> = ({ children, className, ...props }) => {
     return (
         <button
             {...props}
-            className="bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:cursor-not-allowed disabled:text-gray-400 px-6 h-12 rounded-2xl font-medium text-white transition-all flex items-center justify-center"
+            className={cn(
+                'bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:cursor-not-allowed disabled:text-gray-400 px-6 h-12 rounded-2xl font-medium text-white transition-all flex items-center justify-center',
+                className,
+            )}
         >
             {children}
         </button>
