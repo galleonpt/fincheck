ad04b5abc382833261fe5ec072d600fe29cf65f7
galleonpt <jose15.luis00@gmail.com>
2024-03-15 23:43:31 +0000

    feat: style main containers on transactions and accounts

--- frontend/src/view/pages/Dashboard/components/Accounts/Accounts.tsx
+++ frontend/src/view/pages/Dashboard/components/Accounts/Accounts.tsx
@@ -1,7 +1,11 @@
 import { FC } from 'react';
 
 const Accounts: FC = () => {
-    return <div>Accounts</div>;
+    return (
+        <div className="bg-teal-900 rounded-2xl w-full h-full px-4 py-8 lg:p-10 flex flex-col">
+            Accounts
+        </div>
+    );
 };
 
 export default Accounts;
--- frontend/src/view/pages/Dashboard/components/Transactions/Transactions.tsx
+++ frontend/src/view/pages/Dashboard/components/Transactions/Transactions.tsx
@@ -1,7 +1,11 @@
 import { FC } from 'react';
 
 const Transactions: FC = () => {
-    return <div>Transactions</div>;
+    return (
+        <div className="bg-gray-100 rounded-2xl w-full h-full px-4 py-8 lg:p-10 flex flex-col">
+            Transactions
+        </div>
+    );
 };
 
 export default Transactions;
