6dbe1f0a06aaef90edb3398831686171dace8f67
galleonpt <jose15.luis00@gmail.com>
2024-03-15 23:25:27 +0000

    feat: UserMenu

---
+++ frontend/src/view/components/UserMenu.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+const UserMenu: FC = () => {
+    return (
+        <div className="bg-teal-50 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
+            <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">
+                JL
+            </span>
+        </div>
+    );
+};
+
+export default UserMenu;
