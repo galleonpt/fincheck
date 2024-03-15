d793346300168a7e9a2408d7947028286ad8dbf9
galleonpt <jose15.luis00@gmail.com>
2024-03-15 00:17:12 +0000

    feat: add default options for react query queries

--- frontend/src/app/services/httpClient.ts
+++ frontend/src/app/services/httpClient.ts
@@ -1,7 +1,18 @@
 import axios from 'axios';
+import { LOCAL_STORAGE_ACCESS_TOKEN_KEY } from '../config/localStorageKeys';
 
 const httpClient = axios.create({
     baseURL: 'http://localhost:3000',
+});
+
+httpClient.interceptors.request.use(async (config) => {
+    const accessToken = localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY);
+
+    if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
 });
 
 export default httpClient;
