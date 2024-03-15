d696acc545f08173d08130dfc20d492cbe300755
galleonpt <jose15.luis00@gmail.com>
2024-03-15 23:25:48 +0000

    feat: Dashboard header

--- frontend/src/view/pages/Dashboard/Dashboard.tsx
+++ frontend/src/view/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,20 @@
 import { FC } from 'react';
-import useAuth from '../../../app/hooks/useAuth';
+import Logo from '../../components/Logo';
+import UserMenu from '../../components/UserMenu';
 
 const Dashboard: FC = () => {
-    const { logout } = useAuth();
+    return (
+        <div className="w-full h-full p-4 lg:px-8 lg:pt-6 lg:pb-8 flex flex-col gap-4">
+            <header className="h-12 flex items-center justify-between">
+                <Logo className="h-6 text-teal-900" />
+                <UserMenu />
+            </header>
 
-    return <button onClick={logout}>logout</button>;
+            <main className="flex-1 flex flex-col lg:flex-row gap-4 max-h-full">
+                main
+            </main>
+        </div>
+    );
 };
 
 export default Dashboard;
