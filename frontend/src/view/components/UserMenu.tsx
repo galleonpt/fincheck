05e61a9bf71426d8c33c4d0e79b549f62ee8899b
galleonpt <jose15.luis00@gmail.com>
2024-03-27 23:46:02 +0000

    refactor(UserMenu): use the dropdown component

--- frontend/src/view/components/UserMenu.tsx
+++ frontend/src/view/components/UserMenu.tsx
@@ -1,12 +1,31 @@
 import { FC } from 'react';
+import useAuth from '../../app/hooks/useAuth';
+import DropdownMenu from './DropdownMenu';
+import { ExitIcon } from '@radix-ui/react-icons';
 
 const UserMenu: FC = () => {
+    const { logout } = useAuth();
+
     return (
-        <div className="bg-teal-50 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
-            <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">
-                JL
-            </span>
-        </div>
+        <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+                <div className="bg-teal-50 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
+                    <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">
+                        JL
+                    </span>
+                </div>
+            </DropdownMenu.Trigger>
+
+            <DropdownMenu.Content className="w-32">
+                <DropdownMenu.Item
+                    className="flex items-center justify-between"
+                    onSelect={logout}
+                >
+                    Sair
+                    <ExitIcon className="w-4 h-4" />
+                </DropdownMenu.Item>
+            </DropdownMenu.Content>
+        </DropdownMenu.Root>
     );
 };
 
