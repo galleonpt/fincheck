0dc0771a09f0abe7002867c7517cbac6ca559a91
galleonpt <jose15.luis00@gmail.com>
2024-03-27 23:48:57 +0000

    fix: import correct DropdownMenuItem

--- frontend/src/view/components/DropdownMenu/DropdownMenu.tsx
+++ frontend/src/view/components/DropdownMenu/DropdownMenu.tsx
@@ -1,11 +1,11 @@
-import * as RdxDropdownMenu from '@radix-ui/react-dropdown-menu';
 import DropdownMenuRoot from './DropdownMenuRoot';
 import DropdownMenuTrigger from './DropdownMenuTrigger';
 import DropdownMenuContent from './DropdownMenuContent';
+import DropdownMenuItem from './DropdownMenuItem';
 
 export default {
     Root: DropdownMenuRoot,
     Trigger: DropdownMenuTrigger,
     Content: DropdownMenuContent,
-    Item: RdxDropdownMenu.DropdownMenuItem,
+    Item: DropdownMenuItem,
 };
